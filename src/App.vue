e75ea64eb6130e2ad20ff3ecea27dbd6fb8a4e94
Mauricio Alvarez <mauricio.alvarez@utec.edu.pe>
2023-10-20 20:08:21 -0500

    front-end

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
 <template>
   <router-view />
 </template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+console.log(localStorage.getItem('token'))
+
+</script>
---
+++ src/views/Pokemons.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Pokemons</h1>
+        <ul>
+        <li v-for="pokemon in pokemons" :key="pokemon.name">
+            <router-link :to="{ name: 'pokemon', params: { name: pokemon.name } }">
+            {{ pokemon.name }}
+            </router-link>
+        </li>
+        </ul>
+        <router-view />
+    </div>
+</template>
+
+<script setup>
+
+</script>
